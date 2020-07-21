25a8ffd294b601603105aa8b7bb1878c4cb3af38
DJ Draper <darcyjdraper@gmail.com>
2020-07-21 23:13:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["french", 4, false, :eleven]
+  ["elements", 4, false, :eleven]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -11,23 +11,23 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  arry.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  arry[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
